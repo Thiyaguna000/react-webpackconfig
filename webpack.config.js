761d17208d6fd5daaaffc310d5cc157c32a18a73
Thiyagu <thiyaguna000@gmail.com>
2022-07-30 20:31:58 +0100

    react webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index.bundle.js',
+    },
+    devServer: {
+        port: 3010,
+        open: true,
+    hot: true
+    },
+    module: {
+      rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+      ]
+    },
+  };
